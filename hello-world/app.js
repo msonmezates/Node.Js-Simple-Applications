2b87ca19f09882ff7fb964ea04eaa7806b50f4d7
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-14 21:31:28 -0600

    Add a new file and use module exports

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,5 +1,9 @@
 const os = require('os');
 const fs = require('fs');
+const userData = require('./user');
+
+console.log(userData);
+console.log(userData.user.firstName);
 
 let user = os.userInfo();
 let platform = os.platform();
@@ -8,8 +12,10 @@ let date = new Date();
 
 let message = `The user ${user.username} started app at ${date}\n`;
 
-fs.appendFile('hello.txt', message, (err) => {
-  if(err) {
-    console.log('error');
-  }
-});
+if(userData.addLog()) {
+  fs.appendFile('hello.txt', message, (err) => {
+    if(err) {
+      console.log('error');
+    }
+  });
+}
