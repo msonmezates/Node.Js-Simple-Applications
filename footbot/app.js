bbf9888e1ffdd7687c53a98e2f4c4d0c9306e87c
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-17 22:28:08 -0600

    Install command-line-args package and print some values

--- footbot/app.js
+++ footbot/app.js
@@ -1,1 +1,11 @@
-console.log(process.argv);
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [
+  { name: 'name', type: String },
+  { name: 'order', type: String },
+  { name: 'payment', type: Number }
+];
+
+const options = commandLineArgs(optionDefinitions);
+
+console.log(options);
