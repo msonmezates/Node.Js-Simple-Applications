4c3781967f1a58dc9a54eefa318f3e34c471e0dd
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-24 12:28:03 -0600

    Create an HTML file and render it

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hey there, this is a server response')
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  let HTML = fs.readFileSync(`${__dirname}/index.html`)
+  res.end(HTML);
 });
 
 server.listen(8181, 'localhost');
 console.log('Server is running');
+console.log(__dirname);
