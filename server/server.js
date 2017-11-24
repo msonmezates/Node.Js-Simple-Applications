ac354477ed747768eb28351ad6e2256f8609b62f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-24 12:37:48 -0600

    Render some JSON data

--- server/server.js
+++ server/server.js
@@ -2,11 +2,20 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  let HTML = fs.readFileSync(`${__dirname}/index.html`)
-  res.end(HTML);
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+
+  const names = ['Tony', 'Brad', 'Alice'];
+  const cars = {
+    name: 'Ford',
+    model: 'Fiesta'
+  };
+
+  let json = JSON.stringify({
+    names, cars
+  });
+
+  res.end(json);
 });
 
 server.listen(8181, 'localhost');
 console.log('Server is running');
-console.log(__dirname);
