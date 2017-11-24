d8f9b34e194069edbbc80b44993c862de0b46eaf
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-24 12:54:27 -0600

    Create routes for server

--- server/server.js
+++ server/server.js
@@ -2,19 +2,21 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-
-  const names = ['Tony', 'Brad', 'Alice'];
-  const cars = {
-    name: 'Ford',
-    model: 'Fiesta'
-  };
-
-  let json = JSON.stringify({
-    names, cars
-  });
-
-  res.end(json);
+  if(req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const HTML = fs.readFileSync(`${__dirname}/index.html`);
+    res.end(HTML);
+  } else if(req.url === '/api/user') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    const user = JSON.stringify({
+      firstName: 'Mehmet',
+      lastName: 'Sonmezates'
+    });
+    res.end(user);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 });
 
 server.listen(8181, 'localhost');
