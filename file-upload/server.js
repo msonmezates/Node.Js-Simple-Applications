7b16f590181ffb15e862661cf0e49fc4e39accd6
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-11-04 09:19:31 -0600

    Add file upload for single file

---
+++ file-upload/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path'); //built-in node module
+const multer = require('multer');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+////######### HBS SETUP ############/////
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/'
+}));
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.json());
+
+// GET
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.post('/api/uploads',(req, res) =>{
+
+    const upload = multer({ 
+        dest: 'uploads/',
+        limits: { fileSize: 5000000 }, 
+        fileFilter: (req, file, cb) => {
+            const ext = path.extname(file.originalname);
+            ext === '.png' ? cb(null, true) : cb(res.status(400).end('only png is allowed'), false);
+        }
+    }).single('image');
+    
+    upload(req, res, function(err) {
+        if (err) {
+            return res.send(400).end('Error');
+        }
+        return res.end('file successfully uploaded');
+    });
+})
+
+
+app.listen(port,()=>{
+    console.log(`Started on port ${port}`)
+});
