f369e027ad0660b1debbf185bf3b491ec2d72b9f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-01-02 17:29:08 -0600

    Setup redis server and display the list

--- redis-app/app.js
+++ redis-app/app.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser');
 const redis = require('redis');
 const app = express();
 
+// Create client
+const client = redis.createClient();
+
+client.on('connect', () => console.log('Redis server connected'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -15,7 +20,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('hello');
+  let title = 'Task List';
+
+  client.lrange('tasks', 0, -1, (err, reply) => {
+    res.render('index', {
+       title,
+       tasks: reply 
+      });
+  });
 });
 
 app.listen(5000, () => console.log('server started on port 5000'));
