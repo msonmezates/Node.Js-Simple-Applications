1b875f8eafb61f93d1b3cdeff45365ebf552e15a
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-01-18 12:40:59 -0600

    Create a template to send email

--- nodeMailer/index.js
+++ nodeMailer/index.js
@@ -1,11 +1,72 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const nodemailer = require('nodemailer');
 const app = express();
 
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Set static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('hello there');
+  res.render('contact');
+});
+
+app.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+  `;
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: 'test@gmail.com', // generated ethereal user
+        pass: 'abc123' // generated ethereal password
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+      from: '"NodeMailer Contact" <test@gmail.com>', // sender address
+      to: "johndoe@gmail.com", // list of receivers
+      subject: "Hello", // Subject line
+      text: "Test", // plain text body
+      html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+      if(error) {
+        return console.error(error);
+      }
+      console.log("Message sent: %s", info.messageId);
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+      res.render('contact', { msg: "Email has been sent" });
+    });
 });
 
 const PORT = 5000;
