deb272046a54082793c56e1a204d82c784ab016d
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-01-23 08:33:06 -0600

    Add a simple zip folder to run with lambda

---
+++ lambda/index.js
@@ -0,0 +1,9 @@
+const moment = require('moment');
+exports.handler = async (event) => {
+   const min = 1;
+   const max = 6;
+   const randomNumber = Math.floor(Math.random() * (max-min+1)) + min;
+   const now = moment().format();
+   const message = 'The result is ' + randomNumber + ' which was issued at ' + now;
+   return message;
+};
